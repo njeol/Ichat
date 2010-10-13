81c0053cab9da278222a2b01701ab7f8b2d1fd38
njeol <njeol@hypios.com>
2010-10-13 14:32:16 +0200

    cut function

--- serveur_select3.c
+++ serveur_select3.c
@@ -33,29 +33,19 @@ int f_listen(int listener)
   return listener;
 }
 
-int main(int argc, char **argv)
+void f_bind(int listener, struct sockaddr_in serveraddr)
 {
-  fd_set master;
-  fd_set read_fds;
-  struct sockaddr_in serveraddr;
-  struct sockaddr_in clientaddr;
-  int listener, listen = 0;
+  if(bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
+  {
+    perror("Server-bind() error");
+    exit(1);
+  }
+  printf("Server-bind() is OK...\n");
+}
+
+void f_setsockopt(int listener)
+{
   int yes = 1;
-  int fdmax = 0;
-  int addrlen = 0;
-  int i, j, k = 0;
-  int newfd = 0;
-  char *connect = "Hello World\n";
-  char *new_member = "New member connected\n";
-  char *msg_send = "message send\n";
-  char buf[1024];
-  int nbytes = 0;
-  int flag = 0;
-  
-  FD_ZERO(&master);
-  FD_ZERO(&read_fds);
-  
-  listener = f_socket();
   
   if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
   {
@@ -63,18 +53,84 @@ int main(int argc, char **argv)
     exit(1);
   }
   printf("Server-setsockopt() is OK...\n");
+}
+
+int f_select(int fdmax, fd_set read_fds)
+{
+  if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1)
+  {
+    perror("Server-select() error");
+    exit(1);
+  }
+  printf("Server-select() is OK...\n");
+  return fdmax;
+}
+
+void send_msg(int listener, int fdmax, int i, fd_set master)
+{
+  int nbytes = 0;
+  char buf[1024];
+  int j = 0;
+  
+  if ((nbytes = recv(i, buf, sizeof(buf), 0)) >= 0)
+    {
+      for (j = 0; j <= fdmax; j++)
+      {
+      /* send to everyone! */
+        if (FD_ISSET(j, &master))
+        {
+        /* except the listener and ourselves */
+          if (j != listener && j != i)
+          {
+            if (send(j, buf, nbytes, 0) == -1)
+              perror("send() error ");
+          }
+        }
+      }
+    }
+}
+
+int new_member(int flag, int fdmax)
+{
+  int k = 0;
+  char *new_member = "New member connected\n";
+  
+  if (flag == 1)
+   {
+     for (k = 0; k <= fdmax - 1; k++)
+       send(k, new_member, strlen(new_member), 0);
+     flag = 0;
+   }
+   return flag;
+}
+
+void get_init(char **argv)
+{
+  fd_set master;
+  fd_set read_fds;
+  struct sockaddr_in serveraddr;
+  struct sockaddr_in clientaddr;
+  int listener, listen = 0;
+  int fdmax = 0;
+  int addrlen = 0;
+  int i = 0;
+  int newfd = 0;
+  char *connect = "Hello World\n";
+  char *msg_send = "message send\n";
+  int flag = 0;
+  
+  FD_ZERO(&master);
+  FD_ZERO(&read_fds);
+  
+  listener = f_socket();
+  f_setsockopt(listener);
   
   serveraddr.sin_family = AF_INET;
   serveraddr.sin_addr.s_addr = INADDR_ANY;
   serveraddr.sin_port = htons(PORT);
   memset(&(serveraddr.sin_zero), '\0', 8);
-  if(bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
-  {
-    perror("Server-bind() error");
-    exit(1);
-  }
-  printf("Server-bind() is OK...\n");
   
+  f_bind(listener, serveraddr);
   listen = f_listen(listener);
 
   FD_SET(listener, &master);
@@ -82,13 +138,14 @@ int main(int argc, char **argv)
   while(1)
   {
     read_fds = master;
+    // fdmax = f_select(fdmax, read_fds);
     if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1)
     {
       perror("Server-select() error");
       exit(1);
     }
     printf("Server-select() is OK...\n");
-    
+        
     for (i = 0; i <= fdmax; i++)
     {
       if (FD_ISSET(i, &read_fds))
@@ -108,38 +165,18 @@ int main(int argc, char **argv)
             printf("%s: New connection from %s on socket %d\n", argv[0], inet_ntoa(clientaddr.sin_addr), newfd);
             send(newfd, connect, strlen(connect), 0);
           }
-          if (flag == 1)
-           {
-             for (k = 0; k <= fdmax - 1; k++)
-             {
-               send(k, new_member, strlen(new_member), 0);
-             }
-             flag = 0;
-           }
+          flag = new_member(flag, fdmax);
         }
         else
-          {
-             if ((nbytes = recv(i, buf, sizeof(buf), 0)) >= 0)
-              {
-                for (j = 0; j <= fdmax; j++)
-                {
-                /* send to everyone! */
-                  if (FD_ISSET(j, &master))
-                  {
-                  /* except the listener and ourselves */
-                    if (j != listener && j != i)
-                    {
-                      if (send(j, buf, nbytes, 0) == -1)
-                        perror("send() error ");
-                    }
-                  }
-                }
-              }
-          }
-          send(i, msg_send, strlen(msg_send), 0);
+          send_msg(listener, fdmax, i, master);
+        send(i, msg_send, strlen(msg_send), 0);
       }
     }
   }
-  
+}
+
+int main(int argc, char **argv)
+{
+  get_init(argv);  
   return 0;
 } 
