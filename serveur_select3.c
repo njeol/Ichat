9aa61fa273321ebe6dfe67667bf5e4461b04dbce
Belisker <lrodrigues@hypios.com>
2010-10-13 15:34:03 +0200

    add init.c

--- serveur_select3.c
+++ serveur_select3.c
@@ -10,63 +10,6 @@
 
 #define PORT 2011
 
-int f_socket()
-{
-  int listener = 0;
-  
-  if((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-  {
-    perror("Server-socket() error");
-    exit(1);
-  }
-  printf("Server-socket() is OK...\n");
-  return listener;
-}
-
-int f_listen(int listener)
-{
-  if (listen(listener, 10) == -1)
-  {
-    perror("Server-listen() error");
-    exit(1);
-  }
-  printf("Server-listen() is OK...\n");
-  return listener;
-}
-
-void f_bind(int listener, struct sockaddr_in serveraddr)
-{
-  if(bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
-  {
-    perror("Server-bind() error");
-    exit(1);
-  }
-  printf("Server-bind() is OK...\n");
-}
-
-void f_setsockopt(int listener)
-{
-  int yes = 1;
-  
-  if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-  {
-    perror("Server-setsockopt() error");
-    exit(1);
-  }
-  printf("Server-setsockopt() is OK...\n");
-}
-
-int f_select(int fdmax, fd_set read_fds)
-{
-  if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1)
-  {
-    perror("Server-select() error");
-    exit(1);
-  }
-  printf("Server-select() is OK...\n");
-  return fdmax;
-}
-
 void send_msg(int listener, int fdmax, int i, fd_set master)
 {
   int nbytes = 0;
